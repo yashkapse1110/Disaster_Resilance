05547d2781cca98605f4b203a2f1f7c61973b96f
Arun <arunneupane0000@gmail.com>
2025-08-09 17:57:02 +0545

    update: problem in client side routing , fixing with a _redirects file and static file both in public, with build in my local machine ,fix:6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true, // already needed for --host option
+    host: true,
     allowedHosts: [
       "becomes-proceeds-danish-earning.trycloudflare.com",
       "urw-phd-play-relates.trycloudflare.com",
       "nylon-html-suggest-forming.trycloudflare.com",
     ],
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
   },
 });
